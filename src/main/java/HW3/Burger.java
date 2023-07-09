780c570acf90692a43e448a50620b46600d7e360
Oleksandr Popko <oleksandr.popko@monobank.ua>
2023-07-09 20:00:12 +0300

    fixed code to keep DRY principle + changed constructors

--- src/main/java/HW3/Burger.java
+++ src/main/java/HW3/Burger.java
@@ -2,43 +2,41 @@ package HW3;
 
 public class Burger {
     public final String bun;
-    public final String meat;
+    public final int meatQuantity;
     public final String cheese;
     public final String vegetables;
     public final String sause;
+    public Burger(String bun, int meatQuantity, String cheese, String vegetables, String sause) // конструктор
+    {
+        this.bun = bun;
+        this.meatQuantity = meatQuantity;
+        this.cheese = cheese;
+        this.vegetables = vegetables;
+        this.sause = sause;
 
+    }
     public Burger() // стандартний бургер
     {
-        this.bun = "стандартна булочка з кунжутом";
-        this.meat = "Курка";
-        this.cheese = "Чеддер";
-        this.vegetables = "Листок салату та огірок";
-        this.sause = "з майонезом";
+        this("булочка з кунжутом", 1, "Чеддер", "Листок салату та оігрок", "з майонезом");
         displayIngredients();
     }
-    public Burger(String bun, String meat, String cheese, String salad) // дієтичний бургер
+    public Burger(String sause) // дієтичний бургер
     {
-        this.bun = bun;
-        this.meat = meat;
-        this.cheese = cheese;
-        this.vegetables = salad;
-        this.sause = "дієтичний соус";
-        displayIngredients();
-    }
-    public Burger(String bun, String meat, String cheese) // подвійне м'ясо
-    {
-        this.bun = bun;
-        this.meat = meat + " подвійно";
-        this.cheese = cheese;
-        this.vegetables = "";
-        this.sause = "з майонезом";
+        this("булочка з кунжутом",  1 , "Чеддер","Листок салату та огірок", sause);
         displayIngredients();
     }
 
+    public Burger(int meatQuantity) // з подвійним м'ясом
+    {
+        this("Булочка з кунжутом", meatQuantity, "Чеддер", "Листок салату та огірок", "з майонезом");
+        displayIngredients();
+    }
     public void displayIngredients(){
-        System.out.println("Склад вашого бургера:");
+        System.out.println("Склад вашого бургера:"+ "\n");
         System.out.println("Булочка: " + bun);
-        System.out.println("М'ясо: " + meat);
+        if (this.meatQuantity == 1){
+            System.out.println("М'ясо: " + meatQuantity + " котлета");
+        } else System.out.println("М'ясо: " + meatQuantity + " котлети");
         System.out.println("Сир: " + cheese);
         System.out.println("Додатково: " + vegetables);
         System.out.println("Соус: " + sause);
--- src/main/java/HW3/BurgerMain.java
+++ src/main/java/HW3/BurgerMain.java
@@ -3,7 +3,7 @@ package HW3;
 public class BurgerMain {
     public static void main(String[] args) {
         Burger regularBurger = new Burger();
-        Burger dietBurger = new Burger("Булочка без кунжуту", "Курка", "Чеддер", "Помідор та томати");
-        Burger doubleMeatBurger = new Burger("Стандартна булочка з кунжутом", "Яловичина", "Чеддер");
+        Burger dieteticBurger = new Burger("без майонезу");
+        Burger doubleMeatBurger = new Burger(2);
     }
 }
